0bcc29f8cca3bf254d4164a037f0feeae891b5ec
Deby2404 <dedede2404@gmail.com>
2022-04-19 14:03:13 +0000

    finish customer order exept layouts

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,6 +8,6 @@ class Order < ApplicationRecord
   
 
   def add_tax_price
-    (self.tital_price * 1.08).round
+    (customer.total_price * 1.08).round
   end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,6 +3,6 @@ class OrderDetail < ApplicationRecord
   belongs_to :food
   
   def add_tax_price
-    (self.tital_price * 1.08).round
+    (customer.total_price * 1.08).round
   end 
 end
